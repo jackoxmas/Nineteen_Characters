2544eef44f31bb7ab7b4e5f4fd70a099578052d3
Matthew <matthewbregg610@ufl.edu>
2015-02-10 11:32:06 -0500

    Added Ascii art. Added function in Viewport to read in said ascii art. Began the stats view(which is static).

--- src/view/TestingMain.java
+++ src/view/TestingMain.java
@@ -1,5 +1,7 @@
 package src.view;
 
+import src.controller.Avatar;
+
 public class TestingMain {
 
 	public TestingMain() {
@@ -9,10 +11,23 @@ public class TestingMain {
 	/**
 	 * @param args
 	 */
+	public static void printArray(Viewport view){
+		char[][] in = view.getContents();
+		for(int j = 0; j!=view.width_;++j){
+			for(int i = 0; i!=view.length_;++i){
+				{System.out.print(in[i][j]);}
+			}
+			System.out.println();
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Avatar avatar = new Avatar("avatar", 'x', 0, 0);
 		ViewPortTester Tester = new ViewPortTester();
-		System.out.println("Newlin0═════════e");
+		printArray(Tester);
+		System.out.println("Done with viewportTester, ccviewtime!");
+		AvatarCreationView ccview = new AvatarCreationView(avatar);
+		printArray(ccview);
 
 	}
 

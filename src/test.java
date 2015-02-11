d75bd9bc09c8db9a681ee5815e9ba5400d1ccffb
amstewart <diacopium@gmail.com>
2015-02-11 18:02:18 -0500

    Added some test files

---
+++ src/test.java
@@ -0,0 +1,23 @@
+package src;
+import java.io.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author diacopium
+ */
+public class test {
+    public static void main(String[] args)
+    {
+        int BR = 1;
+        int r = 12;
+        if (BR == 0) r = 10; else r = 20;
+        System.out.println(r);
+    }
+}

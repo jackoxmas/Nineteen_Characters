1b0300cbce24a7e366718bfe901bd8b7995e0816
reidholsen <reidh.olsen@ufl.edu>
2015-02-12 12:05:11 -0500

    Update MapEntity_Relation.java
    Implemented several functions. Add a getRespawnPointX/Y() so AvatarMap_Relation could spawn.

--- src/model/MapEntity_Relation.java
+++ src/model/MapEntity_Relation.java
@@ -28,16 +28,53 @@ public class MapEntity_Relation extends MapDrawableThing_Relation {
     private final int x_respawn_point_;
     private final int y_respawn_point_;
     
-    public void spawn(Entity toSpawn, int time_until_spawn) {
+	/**
+	 * This function Spawns an Avatar at the specified Location
+	 * 
+	 * @author reidholsen
+	 */
+	public void spawn(Entity toSpawn, int time_until_spawn) {
+		map_reference_.addEntity(toSpawn, x_respawn_point_, y_respawn_point_);
+	}
 
-    }
+	/**
+	 * This function will move an Entity in the specified direction with the
+	 * specified amount.
+	 * 
+	 * @param toMove
+	 * @param DeltaX
+	 * @param DeltaY
+	 * @author reidholsen
+	 */
+	public void moveInDirection(Entity toMove, int DeltaX, int DeltaY) {
+		int currentX = this.getMyXCordinate();
+		int currentY = this.getMyYCordinate();
 
-    public void moveInDirection(Entity toMove, int DeltaX, int DeltaY) {
+		map_reference_.removeEntity(toMove);
+		map_reference_.addEntity(toMove, currentX + DeltaX, currentY + DeltaY);
+	}
 
-    }
-
-    public void sendAttack(Entity attacker, int x, int y) {
-
+    /**
+	 * Sends an attack in the direction specified by x and y.
+	 * 
+	 * @param attacker
+	 *            - Entity who is doing the attack.
+	 * @param x
+	 *            - x direction of attack
+	 * @param y
+	 *            - y direction of attack
+	 * @author reidholsen
+	 */
+	public void sendAttack(Entity attacker, int x, int y) {
+    	int attack_receiver_x  = attacker.getMapRelation().getMyXCordinate() + x;
+    	int attack_receiver_y  = attacker.getMapRelation().getMyYCordinate() + y;
+    	
+    	try{
+    		Entity attack_receiver = map_reference_.getTile(attack_receiver_x, attack_receiver_y).getEntity();
+    		attack_receiver.getMapRelation().receiveAttack(attack_receiver, 666);
+    	} catch (NullPointerException e){
+    		
+    	}
     }
 
     public void recieveAttack(Entity attack_reciever, int damage) {
@@ -56,7 +93,23 @@ public class MapEntity_Relation extends MapDrawableThing_Relation {
 
     }
     
-    public void entityPickUpItem(Entity entity, Item item) {
-        
-    }
+   	/**
+	 * This function will make Entity entity pick up Item item. 
+	 * Adding item to entity's inventory and deleting item from the map.
+	 * @param entity
+	 * @param item
+	 * @author reidholsen
+	 */
+	public void entityPickUpItem(Entity entity, Item item) {
+		entity.addToInventory(item);
+		item.getMapRelation().removeItem(item);
+	}
+
+	public int getRespawnPointX() {
+		return x_respawn_point_;
+	}
+
+	public int getRespawnPointY() {
+		return y_respawn_point_;
+	}
 }

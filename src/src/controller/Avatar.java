f51c57ed4ffba8bc6cde88352d54fbdb80fb6967
jackoxmas <jackufl@gmail.com>
2015-02-13 23:43:34 -0500

    Initial work on AvatarController and Avatar
    Outlined functionality for how input will work and drive the game.

--- src/src/controller/Avatar.java
+++ src/src/controller/Avatar.java
@@ -22,6 +22,13 @@ public final class Avatar extends Entity {
 
     // map_relationship_ is used in place of a map_referance_
     private final MapAvatar_Relation map_relationship_;
+
+    // holds the views
+    private final Viewport current_viewport_;
+    private final AvatarCreationView avatar_creation_view_;
+    private final MapView map_view_;
+    private final StatsView stats_view_;
+    private char storedInput;
     
     /**
      * Use this to call functions contained within the MapAvatar relationship
@@ -36,12 +43,121 @@ public final class Avatar extends Entity {
     public Avatar(String name, char representation, int x_respawn_point, int y_respawn_point) {
         super(name, representation, x_respawn_point, y_respawn_point);
         map_relationship_ = new MapAvatar_Relation(this, x_respawn_point, y_respawn_point);
+        avatar_creation_view_ = new AvatarCreationView();
+        map_view_ = new MapView();
+        stats_view_ = new StatsView();
+        current_viewport_ = avatar_creation_view_;
     }
 
     private final Display display_ = new Display(new AvatarCreationView(this));
 
     public Display get_my_display() {
         return this.display_;
+    }
+    
+    public void input( char current ) {
+    	if (current_viewport_ != map_view_)
+	    	current_viewport_.input( current );
+    	else {
+    		if (storedInput == 'p') {
+    			switch (current) {
+	    			case '1':
+						map_relationship_.pickUpItemInDirection(0, -1);
+						break;
+					case '2':
+						map_relationship_.pickUpItemInDirection(0, -1);
+						break;
+					case '3':
+						map_relationship_.pickUpItemInDirection(1, -1);
+						break;
+					case '4':
+						map_relationship_.pickUpItemInDirection(-1, 0);
+						break;
+					case '5':
+						map_relationship_.pickUpItemInDirection(0, 0);
+						break;
+					case '6':
+						map_relationship_.pickUpItemInDirection(1, 0);
+						break;
+					case '7':
+						map_relationship_.pickUpItemInDirection(-1, 1);
+						break;
+					case '8':
+						map_relationship_.pickUpItemInDirection(0, 1);
+						break;
+					case '9':
+						map_relationship_.pickUpItemInDirection(1, 1);
+						break;
+    			}
+    		} else if (storedInput == ' ') {
+				switch (current) {
+	    			case '1':
+						map_relationship_.sendAttack(0, -1);
+						break;
+					case '2':
+						map_relationship_.sendAttack(0, -1);
+						break;
+					case '3':
+						map_relationship_.sendAttack(1, -1);
+						break;
+					case '4':
+						map_relationship_.sendAttack(-1, 0);
+						break;
+					case '5':
+						map_relationship_.sendAttack(0, 0);
+						break;
+					case '6':
+						map_relationship_.sendAttack(1, 0);
+						break;
+					case '7':
+						map_relationship_.sendAttack(-1, 1);
+						break;
+					case '8':
+						map_relationship_.sendAttack(0, 1);
+						break;
+					case '9':
+						map_relationship_.sendAttack(1, 1);
+						break;
+				}
+			} else if (storedInput == '~') {
+    			switch (current) {
+	    			case '1':
+						map_relationship_.moveInDirection(0, -1);
+						break;
+					case '2':
+						map_relationship_.moveInDirection(0, -1);
+						break;
+					case '3':
+						map_relationship_.moveInDirection(1, -1);
+						break;
+					case '4':
+						map_relationship_.moveInDirection(-1, 0);
+						break;
+					case '5':
+						map_relationship_.moveInDirection(0, 0);
+						break;
+					case '6':
+						map_relationship_.moveInDirection(1, 0);
+						break;
+					case '7':
+						map_relationship_.moveInDirection(-1, 1);
+						break;
+					case '8':
+						map_relationship_.moveInDirection(0, 1);
+						break;
+					case '9':
+						map_relationship_.moveInDirection(1, 1);
+						break;
+    			}
+    		}
+    		
+    	}
+    	/* determine if input is not important
+    	 * or if we already did something
+    	 * then if true
+    	 * 
+    	 * storedInput = '~';
+    	 */
     }
 
 }
--- src/src/controller/AvatarController.java
+++ src/src/controller/AvatarController.java
@@ -5,6 +5,9 @@
  */
 
 package src.controller;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import src.model.MapAvatar_Relation;
 /**
  * Uses keyboard input to control the avatar
@@ -13,8 +16,23 @@ import src.model.MapAvatar_Relation;
 public final class AvatarController
 {
     private final Avatar my_avatar_;
+    
     AvatarController(Avatar avatar) {
         my_avatar_ = avatar;
+    }
+    
+    private void runTheGame() throws Exception {
+    	
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	int last = 0;
+    	int current = 0;
+    	
+    	while ( (current = in.read() ) != -1 ) {
+			my_avatar_.input((char)last, (char)current);
+			last = current;
+    	}
+    	
+    	in.close();
     }
     
 }

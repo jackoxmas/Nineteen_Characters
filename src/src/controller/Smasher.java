40ebc58b98f13bdee56f38c10557a6e33a6756bc
Jessan Cherian <jessan.cherian@gmail.com>
2015-02-13 20:25:26 -0500

    Updated Occupation

--- src/src/controller/Smasher.java
+++ src/src/controller/Smasher.java
@@ -15,6 +15,7 @@ public final class Smasher implements Occupation
     // Converts the class name into a base 35 number
     private static final long serialVersionUID = Long.parseLong("Smasher", 35);
     public StatsPack change_stats(StatsPack current_stats){
+        //for smasher
         StatsPack level_up_stats = new StatsPack(0,2,1,1,2,0,1,0,1);
         return current_stats.add(level_up_stats);
     }

a8b0a9597de31ed127d68eb9c5cef7abfe8312a9
John-Michael Reed <JohnReedLOL@ufl.edu>
2015-03-07 21:14:28 -0500

    Summoner specific skills

--- src/src/model/map/constructs/Summoner.java
+++ src/src/model/map/constructs/Summoner.java
@@ -5,8 +5,9 @@
  */
 package src.model.map.constructs;
 
+import java.util.Random;
+import src.Effect;
 import src.SkillEnum;
-
 
 /**
  * Summoner Occupation, intellect +1.
@@ -75,8 +76,23 @@ public final class Summoner extends Occupation {
     @Override
     public int performOccupationSkill(int number) {
         if (number == 1) {
+            // influencing another's behavior [Confusion spell]
+            Random randomGenerator = new Random();
+            Boolean isConfused = randomGenerator.nextBoolean();
+            if(isConfused) {
+                super.getEntity().getMapRelation().sendAttackInFacingDirection();
+                super.getEntity().receiveAttack(10, null); // hurt myself
+            } else {
+                super.getEntity().getMapRelation().getEntityInFacingDirection().
+                        receiveAttack(getSkill_1_()*5, null); // hurt enemy [no attack-back]
+            }
         } else if (number == 2) {
+            // boon - magic that heals
+            super.getEntity().getMapRelation().areaEffectFunctor.effectAreaWithinRadius(getSkill_2_() * 2, getSkill_2_(), Effect.HEAL);
+            super.getEntity().getMapRelation().areaEffectFunctor.effectAreaWithinLine(getSkill_2_() * 3, getSkill_2_(), Effect.HURT);
         } else if (number == 3) {
+            // bane - magic that does damage or harm.
+            super.getEntity().getMapRelation().areaEffectFunctor.effectAreaWithinLine(getSkill_3_()*4, 2*getSkill_3_(), Effect.HURT);
         } else if (number == 4) {
             // Staff attack
             final int cost = 1;

9bdff9c5531e364d8ddb7a3b3e94dc6c7f30a670
Matthew <matthewbregg610@ufl.edu>
2015-03-09 20:20:57 -0400

    Moved commandMiniController into it's Hardcoded Strings, and added /man command

--- src/src/HardCodedStrings.java
+++ src/src/HardCodedStrings.java
@@ -24,4 +24,28 @@ public class HardCodedStrings {
     public final static String useItem = "[ Use_Item ]"; // causes the entity to check your inventory for a usable item
     public final static String getItemList = "[ Item ]"; // lists items that this entity will accept
     public final static String trade = "[ Trade ]"; // actually puts the item into your inventory
+    
+    //String from the command mini module
+    public final static String command_error = "Invalid Command";
+    public final static String help = "Type /controls to list all controls" +System.lineSeparator()+
+			"Type /save filename to save" + System.lineSeparator() +
+			"TYpe /save-controls filename to save the current control layout" + System.lineSeparator()+
+			"Note that both save games will save with a date format if no filename is given" + System.lineSeparator()+
+			"Type /rebind command char to rebind a key" + System.lineSeparator()+
+			"Type /load filename to load a file" + System.lineSeparator()+
+			"Type /load-controls filename to load the current controls" + System.lineSeparator()+
+			"Type /help for this dialog"+System.lineSeparator()+
+			"Type /pwd to print the directory the game will look in";
+    public final static String rebindHelp = 
+			"Format is : /rebind CONTROL char" + System.lineSeparator() +
+			"Example : /rebind MOVE_UP u" + System.lineSeparator()+
+			"For commands available for rebinding, type /commands , or type /help for help.";
+	public final static String saveHelp = "Type in /save '[filename]'. The file will then be saved to disk.";
+	public final static String loadHelp= "Type in '/load [filename]'. The file will be loaded from disk";
+	public final static String saveControlsHelp = "Type in '/save-controls [filename]'. The controls will be saved to disk";
+	public final static String loadControlsHelp = "Type in '/load-controls [filename]'. The controls will be loaded from disk";
+	public final static String controlsHelp = "Type in '/controls'. Will list all viable controls that can be rebound via /rebind";
+	public final static String helpHelp = "Type in '/help'. Will list all commands and summaries of them.";
+	public final static String pwdHelp= "Type in '/pwd'. Will print the current directory.";
+	public final static String manHelp= "Type in '/man [commands]'. For information about a command.";
 }
--- src/src/io/controller/CommandMiniController.java
+++ src/src/io/controller/CommandMiniController.java
@@ -2,6 +2,7 @@ package src.io.controller;
 
 import java.util.Scanner;
 
+import src.HardCodedStrings;
 import src.enumHandler;
 
 class CommandMiniController {
@@ -9,21 +10,57 @@ class CommandMiniController {
 	public CommandMiniController(KeyRemapper remap) {
 		remap_ = remap;
 	}
+	private static final String man = "man";
+	private static final String pwd = "pwd";
+	private static final String help = "help";
+	private static final String controls = "controls";
+	private static final String loadControls = "load-controls";
+	private static final String saveControls = "save-controls";
+	private static final String save = "save";
+	private static final String load = "load";
+	private static final String rebind = "rebind";
+	
+	
+	private static final String commandKey = "/";
 
 	public String processCommand(String foo) {
-		if(!foo.startsWith("/")){System.err.println("This isn't a command!"); return "Error in the CommandMini";}
-		if(foo.startsWith("/rebind")){return this.processRebind(foo);}
-		if(foo.startsWith("/save")){return this.processSave(foo);}
-		if(foo.startsWith("/load")){return this.processLoad(foo);}
-		if(foo.startsWith("/save-controls")){return this.processSaveControls(foo);}
-		if(foo.startsWith("/load-controls")){return this.processLoadControls(foo);}
-		if(foo.startsWith("/commands")){return this.processCommands();}
-		if(foo.startsWith("/help")){return this.processHelp();}
-		if(foo.startsWith("/pwd")){return System.getProperty("user.dir");}
-		if(foo.startsWith("/cat")){return "meow";}
-		if(foo.startsWith("/tiger")){return "ROAR!";}
+		if(!foo.startsWith(commandKey)){System.err.println("This isn't a command!"); return "Error in the CommandMini";}
+		if(foo.startsWith(commandKey + rebind)){return this.processRebind(foo);}
+		if(foo.startsWith(commandKey + save)){return this.processSave(foo);}
+		if(foo.startsWith(commandKey + load)){return this.processLoad(foo);}
+		if(foo.startsWith(commandKey + saveControls)){return this.processSaveControls(foo);}
+		if(foo.startsWith(commandKey + loadControls)){return this.processLoadControls(foo);}
+		if(foo.startsWith(commandKey + controls)){return this.processCommands();}
+		if(foo.startsWith(commandKey + help)){return this.processHelp();}
+		if(foo.startsWith(commandKey + pwd)){return System.getProperty("user.dir");}
+		if(foo.startsWith(commandKey + "cat")){return "meow";}
+		if(foo.startsWith(commandKey + "tiger")){return "ROAR!";}
+		if(foo.startsWith(commandKey + man)){return this.processManCommand(foo);}
 
 		return "No valid command given!";
+	}
+
+	private String processManCommand(String foo) {
+		Scanner sc = new Scanner(foo);
+		
+		try{
+			sc.next(); //Get rid of the command /man
+			String in = sc.next();
+
+			if(in.equals(rebind)){return HardCodedStrings.rebindHelp;}
+			if(in.equals(save)){return HardCodedStrings.saveHelp;}
+			if(in.equals(load)){return HardCodedStrings.loadHelp;}
+			if(in.equals(saveControls)){return HardCodedStrings.saveControlsHelp;}
+			if(in.equals(loadControls)){return HardCodedStrings.loadControlsHelp;}
+			if(in.equals(controls)){return HardCodedStrings.controlsHelp;}
+			if(in.equals(help)){return HardCodedStrings.helpHelp;}
+			if(in.equals(pwd)){return HardCodedStrings.pwdHelp;}
+			if(in.equals(man)){return HardCodedStrings.manHelp;}
+			
+		}
+		catch(Exception e){}
+		sc.close();
+		return HardCodedStrings.command_error;
 	}
 
 	private String processLoad(String foo) {
@@ -48,24 +85,12 @@ class CommandMiniController {
 	}
 
 	private String processHelp() {
-		return "Type /command to list all commands" +System.lineSeparator()+
-				"Type /save filename to save" + System.lineSeparator() +
-				"TYpe /save-controls filename to save the current control layout" + System.lineSeparator()+
-				"Note that both save games will save with a date format if no filename is given" + System.lineSeparator()+
-				"Type /rebind command char to rebind a key" + System.lineSeparator()+
-				"Type /load filename to load a file" + System.lineSeparator()+
-				"Type /load-controls filename to load the current controls" + System.lineSeparator()+
-				"Type /help for this dialog"+System.lineSeparator()+
-				"Type /pwd to print the directory the game will look in";
+		return HardCodedStrings.help;
 	}
 
 	private String processRebind(String foo) {
-		String error = "Invalid rebind command given, " + System.lineSeparator()+ 
-				"Format is : /rebind COMMAND char" + System.lineSeparator() +
-				"Example : /rebind MOVE_UP u" + System.lineSeparator()+
-				"For commands available for rebinding, type /commands , or type /help for help.";
+		String error = HardCodedStrings.command_error + System.lineSeparator()+HardCodedStrings.rebindHelp;
 		Scanner sc = new Scanner(foo);
-
 		String command;
 		char c= '\0';
 		try{

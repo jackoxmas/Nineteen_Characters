57b0f8c15f97dcacc1bb8eb01d77ea05048f0b83
John-Michael Reed <JohnReedLOL@ufl.edu>
2015-03-14 14:33:50 -0400

    Do not sent Avatar over the TCP connection

--- src/src/model/map/constructs/Occupation.java
+++ src/src/model/map/constructs/Occupation.java
@@ -16,7 +16,7 @@ import src.SkillEnum;
  */
 public abstract class Occupation implements Serializable {
 
-    private final Entity occupation_holder_;
+    private final transient Entity occupation_holder_;
 
     protected Entity getEntity() {
         return occupation_holder_;

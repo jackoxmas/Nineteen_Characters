ddc49d3d7eae72c87eaed12bb6245906f6651989
Matthew Bregg <matthewbregg610@ufl.edu>
2015-03-21 01:53:21 -0400

    Separated the mapmapeditor_interface into two interfaces, mapmapaddable_interface for the addables, which has the functions just the addables need, and mapmapeditor_interface, which inherits from the addable interface and adds two functions the mapmapeditor needs. Since the mapeditor is needs to pass something that implements the methods of addable_interface to each addable, this makes sense.
    It's not the best, but since the mapmap_editor is second class anyway,
    it's fine, and keeps the map factory and addables from being burdened
    with mapmapeditor stuff

--- src/src/map/editor/MapAddable.java
+++ src/src/map/editor/MapAddable.java
@@ -1,6 +1,6 @@
 package src.map.editor;
 
-import src.model.MapMapEditor_Interface;
+import src.model.MapMapAddable_Interface;
 /**
  * Interface which mapaddables implement.
  * Allows something to take any *Addable, and treat it the same. 
@@ -13,7 +13,7 @@ public interface MapAddable {
 	 * Adds thing to map.
 	 * May need to be called multiple times to get all things on the map added.
 	 */
-	public int addToMap(MapMapEditor_Interface mapp_, int x, int y);
+	public int addToMap(MapMapAddable_Interface mapp_, int x, int y);
 	/**
 	 * True if this adder has nothing left to add
 	 */

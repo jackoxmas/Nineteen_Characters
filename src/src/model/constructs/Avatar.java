6cf07eb3dfde6d92dcd6ac1aedb11cb022c77ecc
Matthew <matthewbregg610@ufl.edu>
2015-03-22 17:30:21 -0400

    Made all avatars red

--- src/src/model/constructs/Avatar.java
+++ src/src/model/constructs/Avatar.java
@@ -5,6 +5,7 @@
  */
 package src.model.constructs;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 import src.HardCodedStrings;
@@ -122,4 +123,6 @@ public final class Avatar extends Entity {
 
         return s;
     }
+    @Override
+    public Color getColor(){return Color.RED;}
 }
